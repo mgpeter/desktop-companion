9a0d2f5dd4a0bc83ee774eed49c85a50cce6da9f
Piotr Wieszynski <mgpeter@gmail.com>
2025-03-30 18:02:33 +0100

    Add Blazor app to Aspire app host

--- ServoSkull.AppHost/Program.cs
+++ ServoSkull.AppHost/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.Extensions.Hosting;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.ServoSkull_ApiService>("apiservice");
+var apiService = builder.AddProject<Projects.ServoSkull_ApiService>("apiservice")
+    .WithEnvironment("ASPNETCORE_ENVIRONMENT", builder.Environment.EnvironmentName);
+
+// Configure ASP.NET Core Web project
+var webApp = builder.AddProject<Projects.ServoSkull_Web>("webapp")
+    .WithReference(apiService)
+    .WithEnvironment("ASPNETCORE_ENVIRONMENT", builder.Environment.EnvironmentName);
 
 // Configure Angular frontend
 var angularApp = builder.AddNpmApp("angular", "../ServoSkull.Angular")
