859631ffd21133d61713fc0d6b5a8e81e8a7ab51
Piotr Wieszynski <mgpeter@gmail.com>
2025-03-31 09:36:05 +0100

    Add initial container setup for the angular app

--- ServoSkull.AppHost/Program.cs
+++ ServoSkull.AppHost/Program.cs
@@ -10,15 +10,30 @@ var webApp = builder.AddProject<Projects.ServoSkull_Web>("webapp")
     .WithReference(apiService)
     .WithEnvironment("ASPNETCORE_ENVIRONMENT", builder.Environment.EnvironmentName);
 
-// Configure Angular frontend
-var angularApp = builder.AddNpmApp("angular", "../ServoSkull.Angular")
-    .WithReference(apiService)
-    .WithHttpEndpoint(env: "PORT")
-    .WithExternalHttpEndpoints()
-    .WaitFor(apiService);
+if (builder.Environment.IsDevelopment())
+{
+    // Configure Angular frontend
+    var angularApp = builder.AddNpmApp("angular", "../ServoSkull.Angular")
+        .WithReference(apiService)
+        .WithHttpEndpoint(env: "PORT")
+        .WithExternalHttpEndpoints()
+        .WaitFor(apiService);
 
-// Get the Angular app's URI and pass it to the API service
-var angularUri = angularApp.GetEndpoint("http");
-apiService.WithEnvironment("FRONTEND_URI", angularUri);
+    // Get the Angular app's URI and pass it to the API service
+    var angularUri = angularApp.GetEndpoint("http");
+    apiService.WithEnvironment("FRONTEND_URI", angularUri);
+}
+else
+{
+    var angularContainerApp = builder.AddContainer("angular", "servoskull-angular")
+        .WithReference(apiService)
+        .WithHttpEndpoint(80)
+        .WithEnvironment("API_URI", apiService.GetEndpoint("http"))
+        .WaitFor(apiService);
+
+    var angularContainerUri = angularContainerApp.GetEndpoint("http");
+    apiService.WithEnvironment("FRONTEND_URI", angularContainerUri);
+
+}
 
 builder.Build().Run();
