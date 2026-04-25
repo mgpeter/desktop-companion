b8c024cd904503dcd6729476403cee95f466e678
Piotr Wieszynski <mgpeter@gmail.com>
2026-04-25 11:23:53 +0100

    Fix container deployment and Aspire setup

--- ServoSkull.AppHost/Program.cs
+++ ServoSkull.AppHost/Program.cs
@@ -13,27 +13,33 @@ var webApp = builder.AddProject<Projects.ServoSkull_Web>("webapp")
 if (builder.Environment.IsDevelopment())
 {
     // Configure Angular frontend
-    var angularApp = builder.AddNpmApp("angular", "../ServoSkull.Angular")
+    var angularApp = builder.AddNpmApp("angular", "../ServoSkull.Angular", "start:aspire")
         .WithReference(apiService)
         .WithHttpEndpoint(env: "PORT")
         .WithExternalHttpEndpoints()
+        .WithEnvironment("BROWSER", "none")
         .WaitFor(apiService);
 
-    // Get the Angular app's URI and pass it to the API service
     var angularUri = angularApp.GetEndpoint("http");
     apiService.WithEnvironment("FRONTEND_URI", angularUri);
 }
 else
 {
+    var apiHttpEndpoint = apiService.GetEndpoint("http");
+
     var angularContainerApp = builder.AddContainer("angular", "servoskull-angular")
         .WithReference(apiService)
-        .WithHttpEndpoint(80)
-        .WithEnvironment("API_URI", apiService.GetEndpoint("http"))
+        .WithHttpEndpoint(targetPort: 80)
+        .WithExternalHttpEndpoints()
+        .WithEnvironment(ctx =>
+        {
+            ctx.EnvironmentVariables["API_HOST"] = apiHttpEndpoint.Property(EndpointProperty.Host);
+            ctx.EnvironmentVariables["API_PORT"] = apiHttpEndpoint.Property(EndpointProperty.Port);
+        })
         .WaitFor(apiService);
 
     var angularContainerUri = angularContainerApp.GetEndpoint("http");
     apiService.WithEnvironment("FRONTEND_URI", angularContainerUri);
-
 }
 
 builder.Build().Run();
